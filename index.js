e75ad116126b9daf1da6bbeb33f48ccba57a6402
Jonathan MASSUCHETTI <jonathan.massuchetti@dappit.fr>
2024-02-27 17:51:55 +0100

    feat: migrate to ESM and add JSDoc with type definitions

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * https://github.com/Pomax/svg-path-reverse
+ *
+ * This code is in the public domain, except in jurisdictions that do
+ * not recognise the public domain, where this code is MIT licensed.
+ */
+import {normalizePath} from './normalizePath.js';
+import {reverseNormalizedPath} from './reverseNormalizedPath.js';
+import {reverseSubPath} from './reverseSubPath.js';
+
+export default {
+  normalize: normalizePath,
+  reverseNormalized: reverseNormalizedPath,
+  reverse: reverseSubPath
+}
